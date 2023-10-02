8037e6eeea8bde5d44d9bc936aaa614192fcde57
rizeenf <rize.nf11@gmail.com>
2023-10-03 00:25:19 +0700

    navbarr

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,27 @@
+import "./Sidebar.scss";
+import { sidebar } from "../../data/sidebar";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {sidebar.map((item) => (
+        <div key={item.id}>
+          <div className="title">{item.title}</div>
+
+          {item.items.map((list) => (
+            <div className="listItem" key={list.id}>
+              <div className="icon">
+                <img src={list.icon} alt="" />
+              </div>
+              <span>{list.title}</span>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Sidebar;
